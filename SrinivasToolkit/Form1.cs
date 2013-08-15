cce90042a5142d3402a0da30469e05058ab260ae
Srinivasa Rao Dhulipalla <srinudhulipalla@gmail.com>
2013-08-15 22:08:45 +0530

    added message box

--- SrinivasToolkit/Form1.cs
+++ SrinivasToolkit/Form1.cs
@@ -16,5 +16,10 @@ namespace SrinivasToolkit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("srinivas");
+        }
     }
 }
