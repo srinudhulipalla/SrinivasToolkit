752607ab5769179f9b239c615c1e5d34f6eecf24
Srinivasa Rao Dhulipalla <srinudhulipalla@gmail.com>
2013-08-15 23:44:29 +0530

    updated

---
+++ SrinivasToolkit/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SrinivasToolkit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.btnCanarysPayslip = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(984, 461);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lineShape1.X1 = 11;
+            this.lineShape1.X2 = 664;
+            this.lineShape1.Y1 = 62;
+            this.lineShape1.Y2 = 62;
+            // 
+            // btnCanarysPayslip
+            // 
+            this.btnCanarysPayslip.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnCanarysPayslip.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCanarysPayslip.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnCanarysPayslip.Image = global::SrinivasToolkit.Properties.Resources.canarys_logo;
+            this.btnCanarysPayslip.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCanarysPayslip.Location = new System.Drawing.Point(71, 104);
+            this.btnCanarysPayslip.Name = "btnCanarysPayslip";
+            this.btnCanarysPayslip.Size = new System.Drawing.Size(202, 65);
+            this.btnCanarysPayslip.TabIndex = 2;
+            this.btnCanarysPayslip.Text = "Parse eCanarys Pay-slip";
+            this.btnCanarysPayslip.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCanarysPayslip.UseVisualStyleBackColor = false;
+            this.btnCanarysPayslip.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(124, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Srinivas Software Toolkit";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.btnCanarysPayslip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Srinivas Software Toolkit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Button btnCanarysPayslip;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- SrinivasToolkit/eCanarysPayslipParser.Designer.cs
+++ SrinivasToolkit/eCanarysPayslipParser.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SrinivasToolkit
 {
-    partial class Form1
+    partial class eCanarysPayslipParser
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // eCanarysPayslipParser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.button1);
+            this.Name = "eCanarysPayslipParser";
+            this.Text = "eCanarysPayslipParser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.eCanarysPayslipParser_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
-
