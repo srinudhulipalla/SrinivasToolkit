8a7971e5dd9a8dbc351a53be0a1748f1be618be6
Srinivasa Rao Dhulipalla <srinudhulipalla@gmail.com>
2013-08-16 15:36:10 +0530

    rework

--- SrinivasToolkit/Business/eCanarys.cs
+++ SrinivasToolkit/Business/eCanarys.cs
@@ -1,11 +1,11 @@
-﻿using Aspose.Pdf;
-using Aspose.Pdf.Text;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
 
 namespace SrinivasToolkit.Business
 {
